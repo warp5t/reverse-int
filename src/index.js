0446b9d675fe5cbc67baf6509a6553abb7969446
warp5t <dmitrij.didencko@yandex.by>
2024-08-09 07:34:05 +0300

    init: sovle task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    let initNumber = n;
+    if (n < 0) initNumber = -initNumber
+    const stringReverse = String(initNumber).split('').reverse().join('');
+    return Number(stringReverse)
 }
